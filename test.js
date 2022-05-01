ac2cb37bc490c07f2153224f744ca6c111b0230f
Ken Warner <ken.c.warner@gmail.com>
2022-05-01 10:17:48 -0400

    Created code windows for each card

--- test.js
+++ test.js
@@ -1,13 +1,55 @@
+//get a reference to the template and the container to put them in
 var cardTemplate = document.getElementById('card-template');
-let container = document.getElementById('container');
+var container = document.getElementById('container');
 
+//create a bunch of containers for testing
 for (let i = 0; i < 10; i++) {
-    let clone = cardTemplate.content.firstElementChild.cloneNode(true);
-    let header = clone.querySelector('h3.card-header');
-    let cont = clone.querySelector('div.content');
+    var clone = cardTemplate.content.firstElementChild.cloneNode(true);
+    var header = clone.querySelector('h3.card-header');
+    var cont = clone.querySelector('div.content');
 
     cont.innerHTML = "<button>Testing</button>";
     header.appendChild(document.createTextNode("lorem ipsum"));
 
     container.appendChild(clone);
 }
+
+//add event handlers to all the close buttons
+var closeButtons = document.querySelectorAll(".close-button");
+closeButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.target.parentElement.style.display = "none";
+    });
+});
+
+//All of the buttons in each container
+var codeButtons = document.querySelectorAll('[data-button]');
+
+//Put event handlers for each button to show their respective codes
+codeButtons.forEach(button => {
+    //there are different handlers for each type of button
+    if (button.dataset.button == "html") {
+        button.addEventListener('click', (e) => {
+            e.target.parentElement
+                    .parentElement
+                    .querySelector('[data-code-html]')
+                    .style.display = 'block';
+        });
+    } else if (button.dataset.button == "css") {
+        button.addEventListener('click', (e) => {
+            e.target.parentElement
+                    .parentElement
+                    .querySelector('[data-code-css]')
+                    .style.display = 'block';
+        });
+    } else { //must be js
+        button.addEventListener('click', (e) => {
+            e.target.parentElement
+                    .parentElement
+                    .querySelector('[data-code-js]')
+                    .style.display = 'block';
+        });
+    }
+});
+
+
